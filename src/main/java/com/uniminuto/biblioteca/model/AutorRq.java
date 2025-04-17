9c63aebcf6b1fe74813e2a6cffd439fc39210da8
khidalgo <kalethhidalgo95@gmail.com>
2025-04-16 21:15:33 -0500

    cambios

---
+++ src/main/java/com/uniminuto/biblioteca/model/AutorRq.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uniminuto.biblioteca.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author kalet
+ */
+public class AutorRq {
+
+    public LocalDate getFechaNacimiento() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getNacionalidad() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/com/uniminuto/biblioteca/model/AutorRs.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uniminuto.biblioteca.model;
+
+/**
+ *
+ * @author kalet
+ */
+public class AutorRs {
+
+    public void setMessage(String se_ha_guardado_el_autor_con_éxito) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
