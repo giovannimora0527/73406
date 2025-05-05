6de112b9ec58820ff93f5519eb7876de99ee2db8
khidalgo <kalethhidalgo95@gmail.com>
2025-05-05 11:56:29 -0500

    ayuda

--- src/main/java/com/uniminuto/biblioteca/repository/PrestamoRepository.java
+++ src/main/java/com/uniminuto/biblioteca/repository/PrestamoRepository.java
@@ -1,4 +1,16 @@
-
+/**
+ * Repositorio para gestionar las operaciones de acceso a los datos relacionados con los préstamos.
+ * 
+ * <p>Esta interfaz extiende de {@link JpaRepository}, proporcionando métodos CRUD estándar
+ * para la entidad {@link Prestamo}. Los métodos definidos aquí permiten interactuar con la base de datos
+ * para realizar operaciones sobre los registros de préstamos.</p>
+ * 
+ * <p>El repositorio está diseñado para trabajar con la entidad {@link Prestamo} y su identificador
+ * de tipo {@link Integer}.</p>
+ * 
+ * @author kaleth
+ * @version 1.0
+ */
 package com.uniminuto.biblioteca.repository;
 
 import com.uniminuto.biblioteca.entity.Prestamo;
