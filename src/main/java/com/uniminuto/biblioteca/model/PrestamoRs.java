94923dbeaee671ca2848cf42244cd89fcb4ccc86
khidalgo <kalethhidalgo95@gmail.com>
2025-05-05 11:56:16 -0500

    ayuda

--- src/main/java/com/uniminuto/biblioteca/model/PrestamoRs.java
+++ src/main/java/com/uniminuto/biblioteca/model/PrestamoRs.java
@@ -1,4 +1,14 @@
-
+/**
+ * Representa la respuesta de una operación de préstamo en el sistema de biblioteca.
+ * 
+ * <p>Esta clase contiene el mensaje que se devuelve como respuesta después de
+ * realizar una acción relacionada con un préstamo, como crear o actualizar un préstamo.</p>
+ * 
+ * <p>Es utilizada para enviar información al usuario sobre el resultado de la operación.</p>
+ * 
+ * @author kaleth
+ * @version 1.0
+ */
 package com.uniminuto.biblioteca.model;
 
 import lombok.Data;
